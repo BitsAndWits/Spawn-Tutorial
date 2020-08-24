633d2f2cff63d1f7b3ce3c078f7825930e9d244e
Abd Majeed Abd Mumuni <38582706+BitsAndWits@users.noreply.github.com>
2020-08-24 14:54:41 +0100

    Add files via upload

---
+++ SpawnCharacters.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCharacters : MonoBehaviour
+{
+    public GameObject[] characters;
+    Vector3 point;
+
+    void Update()
+    {
+        RaycastHit raycast;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        if (Physics.Raycast(ray, out raycast))
+        {
+            point = raycast.point;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnCharacter(point);
+        }
+    }
+
+    void SpawnCharacter(Vector3 point)
+    {
+        Instantiate(characters[Random.Range(0, characters.Length - 1)], point, Quaternion.Euler(0f, 180f, 0f));
+    }
+}
---
+++ SpawnLogo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLogo : MonoBehaviour
+{
+    public GameObject logo;
+    Vector2 point;
+
+    void Update()
+    {
+        point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Spawn(point);
+        }
+    }
+
+    void Spawn(Vector2 point)
+    {
+        Instantiate(logo).transform.position = point;
+    }
+}
